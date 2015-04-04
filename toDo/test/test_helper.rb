c14a5704280d0cc0697c8ebbd43973b237ee1069
Matthias Karl <itgkarl+git@gmail.com>
2015-04-04 07:03:50 -0700

    Webanwendung als eigenes Projekt ausgelagert.

---
+++ toDo/test/test_helper.rb
@@ -0,0 +1,24 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+    add_filter '/config/'
+    add_filter '/lib/'
+    add_filter '/vendor/'
+    
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Models', 'app/models'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Mailers', 'app/mailers'
+    add_group 'Views', 'app/views'
+end
+
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
